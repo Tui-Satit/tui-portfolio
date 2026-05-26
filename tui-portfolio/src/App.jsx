5ffa737db754f90394d0c539df05e4cba326de1e
Tui <m.satit@gmail.com>
2026-05-26 15:57:37 +0700

    First portfolio website

---
+++ tui-portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="app">
+      <section className="hero">
+        <p className="hero-tag">
+             👋 Hello, I'm
+        </p>
+     
+        <h1>Tui Portfolio</h1>
+
+        <h2>
+          Frontend React Developer
+        </h2>
+
+        <p className="hero-desc">
+           I build modern wesite for small businesses,
+           coffee shops and portfolio websites.
+        </p>
+
+        <button>
+          View My Work
+        </button>
+      </section>
+    </div>
+  )
+}
+
+export default App;
