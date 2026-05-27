8f8ff4aa87f610dc0133b808bbb82849bf92a1cd
Tui <m.satit@gmail.com>
2026-05-27 16:35:09 +0700

    improve link on navbar

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="navbar">
+          <h3>Tui</h3>
+
+          <div className="nav-links">
+          <a href="#home">Home</a>
+           <a href="#projcts">Projects</a>
+            <a href="#contact">Contact</a>
+            <button className="line-btn">
+               <a href="https://line.me/ti/p/@575kncik">LINE chat</a>
+            </button>
+            </div>
+            
+      </nav>
+
+      <main className="app">
+          <section className="hero">
+        <p className="hero-tag">
+             👋 Hello, I'm
+        </p>
+     
+        <h1>Tui Portfolio</h1>
+
+        <h2>
+          Frontend React Developer
+        </h2>
+
+        <p className="hero-desc">
+           I build modern wesite for small businesses,
+           coffee shops and portfolio websites.
+        </p>
+
+        <button>
+          View My Work
+        </button>
+      </section>
+
+      <section className="projects" id="projects">
+        <h2>Projects</h2>
+
+        <div className="project-grid">
+          <div className="project-card">
+            <img src="/images/coffee-shop.jpg" alt="Coffee Shop" />
+            <h3>Coffee Shop Website</h3>
+
+            <p>
+              Modern coffee ordering website with Firebase realtime orders and
+              LINE integration.
+            </p>
+            <a href="https://coffee-shop-website.vercel.app"
+            target="_blank"
+            >
+             Live Demo
+            </a>
+          </div>
+            
+          <div className="project-card">
+ <img src="/images/sign-shop.jpg" alt="Sign Shop Website" />
+
+             <h3>Sign Shop Website</h3>
+
+             <p>
+              Clean business website for alocal sign shop with services, 
+              portfolio and contact links. 
+
+             </p>
+
+             <a href="https://sign-shop.vercel.app/" target="_blank">
+
+             Live Demo 
+             </a>
+          </div>
+        </div>
+      </section>
+
+      </main>
+     
+    </div>
+  )
+}
+
+export default App;
